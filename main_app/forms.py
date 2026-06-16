f069cdd9163096b82b819afc5eaf5141f7e772fa
Ronnie95 <t_daron@yahoo.com>
2026-06-16 11:41:16 -0400

    form updates

---
+++ main_app/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Shift
+
+class ShiftReport(forms.ModelForm):
+    class Meta:
+        model = Shift
+        fields = [
+            'date',
+            'shift_name',
+            'people_amount',
+            'breakdowns',
+            'assistance_needed',
+            'additional_comments'
+        ]
+
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'type': 'date'
+            }),
+            'breakdowns': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Describe any breakdowns...'
+            }),
+            'additional_comments': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Additional notes for next shift...'
+            }),
+        }
